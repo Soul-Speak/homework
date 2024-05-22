1513b9b3534cacf392415dc83158d23de5e08b01
Soul-Speak <166742863+Soul-Speak@users.noreply.github.com>
2024-05-22 22:35:18 +0300

    add lesson-76

---
+++ lesson-76/main.js
@@ -0,0 +1,7 @@
+const firstName = 'Dima'
+const lastName = 'Poluyanov'
+const age = '18'
+const height = '170cm'
+const penisLenght = '17cm'
+
+console.log(firstName, lastName, age, 'y.o', height, penisLenght)
