68e7e8b35a91d00c3281bcf17d25d1aecb55b98a
Soul-Speak <166742863+Soul-Speak@users.noreply.github.com>
2024-05-24 13:03:13 +0300

    add lesson-80

---
+++ lesson-80/main.js
@@ -0,0 +1,20 @@
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(i)
+}
+
+for (let i1 = 10; i1 >= 1; i1 = i1 - 1) {
+    console.log(i1)
+}
+
+
+let i2 = 0
+while(i2 < 12) {
+    i2 = i2 + 2
+    console.log(i2)
+}
+
+let i3 = 11
+while(i3 >= 2) {
+    i3 = i3 - 1
+    console.log(i3)
+}
