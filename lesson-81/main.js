9e3418fc0128d50041e45e8feaa72ce382117410
Soul-Speak <166742863+Soul-Speak@users.noreply.github.com>
2024-05-30 18:42:40 +0300

    add lesson-81

---
+++ lesson-81/main.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7]
+
+for(let i = 2; i < numbers.length; i +=2) {
+    console.log(i)
+}
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'light-blue', 'blue', 'purple']
+for(let i2 = 6; i2 > -1; i2 = i2 - 1) {
+    console.log(rainbow[i2])
+}
