a145fcf041fd2e0cb866e5f8203ffc956517f037
Soul-Speak <166742863+Soul-Speak@users.noreply.github.com>
2024-06-04 22:57:48 +0300

    Update main.js

--- lesson-82/main.js
+++ lesson-82/main.js
@@ -4,7 +4,9 @@ function greeting(name = 'Avgan') {
 
 const result = greeting()
 console.log(result)
+
 // =========================================================================
+
 const numbers = [1, 3, 7, 12, 22, 228, 1337, 1488]
 const numbers2 = []
 
@@ -17,3 +19,25 @@ function checkforNumbersAbove10(array, item) {
     return `${numbers2}`
 }
 console.log(checkforNumbersAbove10(numbers2, 10))
+
+// =========================================================================
+
+function calculator(firstNumber, secondNumber, operation) {
+    
+    if (operation === 'plus') {
+        return firstNumber + secondNumber
+    } else if (operation === 'minus') {
+        return firstNumber - secondNumber
+    } else if (operation === 'multiply') {
+        return firstNumber * secondNumber
+    } else if (operation === 'divide') {
+        return firstNumber / secondNumber
+    } else {
+        return 'invalid (ti)'
+    }
+    
+}
+
+const result1 = calculator(55, 19, 'divide')
+
+console.log(result1)
